2daf8f81689c99d703bdd6cb0747378ef28138dd
israt6898 <israt6898@gmail.com>
2024-08-15 22:18:43 +0600

    finish banner section

---
+++ src/Components/Home/Home/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+
+
+const Banner = () => {
+    return (
+        <div className="my-[40px]">
+            <div
+  className="hero h-[400px]"
+  style={{
+    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold text-red-500">WellCome Style House</h1>
+      <p className="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+    <button className="btn btn-primary">Explore Now</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
