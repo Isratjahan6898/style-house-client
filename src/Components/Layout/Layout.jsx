cc815120780473a73cd27a74c8966757484871e9
israt6898 <israt6898@gmail.com>
2024-08-15 23:22:46 +0600

    finish footer section

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Home/Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 
 const Layout = () => {
@@ -9,6 +10,7 @@ const Layout = () => {
            <div>
             <Outlet></Outlet>
            </div>
+           <Footer></Footer>
         </div>
     );
 };
